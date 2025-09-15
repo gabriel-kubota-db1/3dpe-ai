c4c88e769ce647280eb00b5485aaba54d09b8193
gabriel-kubota-db1 <gabriel.kubota@db1.com.br>
2025-09-15 15:14:23 -0300

    unifying insole

--- backend/src/domains/insole-models/model.ts
+++ backend/src/domains/insole-models/model.ts
@@ -3,6 +3,13 @@ import { Model } from 'objection';
 export class InsoleModel extends Model {
   id!: number;
   description!: string;
+  type!: string;
+  numeration!: string;
+  coating_type!: 'eva' | 'fabric';
+  eva_coating_id?: number | null;
+  fabric_coating_id?: number | null;
+  cost_value!: number;
+  sale_value!: number;
   active!: boolean;
 
   static get tableName() {
