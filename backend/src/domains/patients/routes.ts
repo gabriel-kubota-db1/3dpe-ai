9c87674c08ed66b206e27a67e1cd0487f886b9e6
Gabriel Kubota <gabriel.kubota@db1.com.br>
2025-09-17 09:14:56 -0300

    fixing validators

--- backend/src/domains/patients/routes.ts
+++ backend/src/domains/patients/routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import * as controller from './controller';
 import { validateRequest } from '../../middlewares/validateRequest';
-import { patientSchema } from './validators';
+import { createPatientSchema, updatePatientSchema } from './validators';
 import { isAuthenticated } from '../../middlewares/isAuthenticated';
 import { isPhysiotherapist } from '../../middlewares/isPhysiotherapist';
 
@@ -10,9 +10,9 @@ const router = Router();
 router.use(isAuthenticated, isPhysiotherapist);
 
 router.get('/', controller.getAllPatients);
-router.post('/', validateRequest({ body: patientSchema }), controller.createPatient);
+router.post('/', validateRequest({ body: createPatientSchema }), controller.createPatient);
 router.get('/:id', controller.getPatientById);
-router.put('/:id', validateRequest({ body: patientSchema }), controller.updatePatient);
+router.put('/:id', validateRequest({ body: updatePatientSchema }), controller.updatePatient);
 router.get('/:id/audit-logs', controller.getPatientAuditLogs);
 
 export default router;
