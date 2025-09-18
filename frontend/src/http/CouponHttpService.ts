980236baa51714eb81e8a55ad851b4ef63d07da1
Gabriel Kubota <gabriel.kubota@db1.com.br>
2025-09-18 16:37:03 -0300

    fix error

--- frontend/src/http/CouponHttpService.ts
+++ frontend/src/http/CouponHttpService.ts
@@ -5,3 +5,8 @@ export const validateCoupon = async (code: string): Promise<Coupon> => {
   const { data } = await api.post('/coupons/validate', { code });
   return data;
 };
+
+export const getCoupons = (): Promise<Coupon[]> => api.get('/coupons').then(res => res.data);
+export const createCoupon = (data: Omit<Coupon, 'id'>): Promise<Coupon> => api.post('/coupons', data).then(res => res.data);
+export const updateCoupon = (id: number, data: Partial<Coupon>): Promise<Coupon> => api.put(`/coupons/${id}`, data).then(res => res.data);
+export const deleteCoupon = (id: number): Promise<void> => api.delete(`/coupons/${id}`);
